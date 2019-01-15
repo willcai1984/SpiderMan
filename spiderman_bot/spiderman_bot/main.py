439664458631e7b75c37a65ad15e2df61d20fa35
caiwei <caiwei@qjdchina.com>
2019-01-15 09:56:20 +0800

    init version

--- spiderman_bot/spiderman_bot/main.py
+++ spiderman_bot/spiderman_bot/main.py
@@ -5,9 +5,16 @@
 """
 import sys
 import os
-from scrapy.cmdline import execute
+
+# from scrapy.cmdline import execute
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
-execute(["scrapy", "crawl", "hzcz"])  # 这句代码会执行爬虫类中 name = "myspider_haha"的类
-execute(["scrapy", "crawl", "zjzfcg"])
-execute(["scrapy", "crawl", "email"])
+# execute方法中的最后一句代码是sys.exit(cmd.exitcode)，所以当程序执行完一个execute语句后便停止了。
+# https://stackoverflow.com/questions/24875280/scrapy-cmdline-execute-stops-script
+# execute(["scrapy", "crawl", "hzcz"])  # 这句代码会执行爬虫类中 name = "myspider_haha"的类
+# execute(["scrapy", "crawl", "zjzfcg"])
+# execute(["scrapy", "crawl", "email"])
+
+os.system('scrapy crawl hzcz')
+os.system('scrapy crawl zjzfcg')
+os.system('scrapy crawl email')
